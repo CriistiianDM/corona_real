96c45ea43ea3431d86a6c6830605641abe395400
Cristian Machado <cristian.machado@correounivalle.edu.co>
2024-09-08 03:26:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CORONA REAL
+
+## INSTALL WHIT DOCKER
+
+### DEBIAN:
+```
+    docker compose up --build 
+```
+
+### Ubuntu
+```
+    docker-compose up --build 
+```
---
+++ src/server/server/signals.py
@@ -0,0 +1,22 @@
+# src/server/backend/signals.py
+
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_default_superuser(sender, **kwargs):
+    """
+    Crea superusuarios por defecto si no existen.
+    """
+    User = get_user_model()
+    users = [
+        {"username": "cristiank", "password": "MBg3hZwdHb525qjUw", "email": "cristian@corporation.com"},
+    ]
+
+    for u in users:
+        if not User.objects.filter(username=u["username"]).exists():
+            User.objects.create_superuser(u["username"], u["email"], u["password"])
+            print(f'Superuser created: {u["username"]}')
+        else:
+            print(f'Superuser already exists: {u["username"]}')
