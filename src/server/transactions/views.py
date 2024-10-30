d805a0e4d21e12cc8632bef5a9e94dad3e9772ba
LeninCar <lenin.carabali@correounivalle.edu.co>
2024-10-30 01:12:35 -0500

    Cambio de modelo de rooms, crud transacciones y seeders de tablas

--- src/server/transactions/views.py
+++ src/server/transactions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from .models import TypesTransactions, TypesCashRegister, CashRegister, Transactions
+from .serializers import (
+    TypesTransactionsSerializer, 
+    TypesCashRegisterSerializer, 
+    CashRegisterSerializer, 
+    TransactionsSerializer
+)
+
+class TypesTransactionsViewSet(viewsets.ModelViewSet):
+    queryset = TypesTransactions.objects.all()
+    serializer_class = TypesTransactionsSerializer
+
+class TypesCashRegisterViewSet(viewsets.ModelViewSet):
+    queryset = TypesCashRegister.objects.all()
+    serializer_class = TypesCashRegisterSerializer
+
+class CashRegisterViewSet(viewsets.ModelViewSet):
+    queryset = CashRegister.objects.all()
+    serializer_class = CashRegisterSerializer
+
+class TransactionsViewSet(viewsets.ModelViewSet):
+    queryset = Transactions.objects.all()
+    serializer_class = TransactionsSerializer
