d537ff8792f2d3ae96dac9efb03733732d3dd8e2
Cristian Machado <cristian.machado@correounivalle.edu.co>
2024-11-09 23:02:51 -0500

    add token validate and route for get

--- src/server/person/urls.py
+++ src/server/person/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('accounts/', views.post_create_accounts, name='account'),
+    path('filter_name_identification/', views.get_person_by_name_or_identification, name='filter_name_identification'),
+    path('get_person_by_type/', views.get_person_by_type, name='person_by_type'),
     path('____/', views.get_csrf_token, name='token'),
 ]
