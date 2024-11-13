2a50a136c602aad1423a85cd00f579bef422644f
Cristian Machado <cristian.machado@correounivalle.edu.co>
2024-11-12 23:41:34 -0500

    add conection front-back and implements login and create in frotend methods

---
+++ Frontend/.conf.js
@@ -0,0 +1,7 @@
+export default {
+    "URL": "http://127.0.0.1:8000",
+    "csrftToken": "/api/person/____/",
+    "authorization": "/api/token/",
+    "login": "/api/person/login/",
+    "create": "/api/person/accounts/"
+}
