81e41a87f052e139a8738490834cd282f6b27f83
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2024-12-02 13:11:22 -0500

    Update settings.py

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -66,7 +66,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 ROOT_URLCONF = 'server.urls'
 CSRF_COOKIE_AGE = 60 
